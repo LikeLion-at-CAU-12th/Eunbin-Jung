a7f7cac63a68941545b5a7a09353b16531a1a614
eunkong0-0 <joyjessica@naver.com>
2024-04-30 05:26:31 +0900

    Feat : 버튼에 Icon 추가하기

--- week6/DOM/Button.js
+++ week6/DOM/Button.js
@@ -1,8 +1,13 @@
 import DOM from "./DOM.js";
+import Img from "./Img.js";
 
-class Button extends DOM{
-  constructor(innerText, className){
-    super('button', innerText, className);
+class Button extends DOM {
+  constructor(iconSrc, className) {
+    super('button', '', className);
+    if (iconSrc) {
+      this.icon = new Img(iconSrc, 'Button icon', 'button-icon').node;
+      this.node.appendChild(this.icon);
+    }
   }
 }
 
--- week6/DOM/Todo.js
+++ week6/DOM/Todo.js
@@ -1,13 +1,12 @@
 import Button from "./Button.js";
 import Div from "./Div.js";
 
-
 class Todo{
   constructor(todo){
     this.row = new Div('', 'row').node;
     this.textBox = new Div(todo, 'text-box');
-    this.completeBtn = new Button('완료', 'complete-btn');
-    this.delBtn = new Button('삭제', 'del-Btn');
+    this.completeBtn = new Button('images/complete.png', 'complete-btn');
+    this.delBtn = new Button('images/delete.png', 'del-Btn');
   }
   addRow(){
     [this.textBox, this.completeBtn, this.delBtn].forEach((dom)=>{
