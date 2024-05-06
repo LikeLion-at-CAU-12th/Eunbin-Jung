becf762f29a65cabac55fec1022b71e0b06cdd09
eunkong0-0 <joyjessica@naver.com>
2024-05-07 02:39:47 +0900

    Feat : 파일 업로드

---
+++ week7/prac1/filter.js
@@ -0,0 +1,27 @@
+const url = './data/data.json';
+const container = document.getElementById('container');
+
+const filterData = () =>{
+  while(container.firstChild){
+    container.removeChild(container.firstChild); //모두 불러오기 버튼 여러 번 눌러도 한 번만 나오게 첫번째 자식 요소 삭제
+  }
+  fetch(url)
+  .then((response)=>{
+    return response.json()
+  })
+  .then((response)=>{
+    console.log(response.frontend); //배열만 출력하기 위해 .frontend 추가
+    const datas = response.frontend; //frontend 변수 설정
+    
+    datas
+    .filter((data)=> data.role == '아기사자')
+    .map((data)=>{
+      const list = document.createElement('div');
+      list.innerHTML = `제 이름은 ${data.name}입니다.
+      저는 ${data.role}입니다. 그리고 제 전공은 ${data.major}입니다~`;
+
+      container.appendChild(list);
+      console.log(data);
+    })
+  })
+}
---
+++ week7/prac2/index.js
@@ -0,0 +1,54 @@
+const baseURL = "https://apis.data.go.kr/B551011/PhotoGalleryService1";
+
+const container = document.getElementById("container");
+
+const option = {
+  serviceKey:
+    "G%2FbsC7xgzNTc8TzbcaNz0jeZ0CUvE8HB3rUTN7069PrIQ9hVvIqbAkOfauW%2F2X%2FR15%2FEL5eSOUvOlAZ659dn3w%3D%3D",
+  numofRows: 5,
+  MobileApp: "test",
+  MobileOS: "ETC",
+  arrange: "A",
+  _type: "json",
+  pageNo : 1
+};
+
+let count = -1; //계속 변하는 값이니까 const가 아니라 let으로 선언해야 함
+
+async function getData(){
+  // const random = Math.floor(Math.random()*100 +1);
+  const url = `${baseURL}/galleryList1?numOfRows=${option.numofRows}&MobileApp=${option.MobileApp}&MobileOS=${option.MobileOS}&arrange=${option.arrange}&_type=${option._type}&pageNo=${option.pageNo}&serviceKey=${option.serviceKey}`;
+  
+  count ++;
+  const fetchData = await fetch(url);
+  console.log(fetchData);
+
+  const toJson = await fetchData.json();
+  console.log(toJson);
+
+  const datas = await toJson.response.body.items.item;
+  console.log(datas);
+
+  datas.map((data, i)=>{
+    const list = document.createElement('div');
+    list.id = 'list';
+
+    const image = document.createElement('img');
+    image.src = data.galWebImageUrl;
+
+    const info = document.createElement('span');
+    info.innerText = `  
+    📸${i + 1 + 5 * count}번째 사진
+    제목 : ${data.galTitle}
+    장소 : ${data.galPhotographyLocation}`;
+
+    const button = document.createElement('button');
+    button.innerText = "더보기";
+
+    list.appendChild(image);
+    list.appendChild(info);
+    list.appendChild(button);
+
+    container.appendChild(list);
+  })
+}
