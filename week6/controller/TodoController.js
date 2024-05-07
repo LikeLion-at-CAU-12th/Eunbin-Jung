4c3c7fe803ecfaf8254c16f9d044841b7f0d9200
eunkong0-0 <joyjessica@naver.com>
2024-05-07 18:09:13 +0900

    Feat : week6 completeList 삭제 버튼 구현

--- week6/controller/TodoController.js
+++ week6/controller/TodoController.js
@@ -7,13 +7,23 @@ class TodoController {
     this.comBtnNode = this.newTodo.getCompleteBtn();
     this.innerNode = this.newTodo.getInnerText();
 
+
     this.delBtnNode.addEventListener('click',()=>{
-      this.delTodo();
+      // newTodo의 부모 요소 ID를 가져옴
+      const parentId = this.newTodo.getRow().parentNode.id;
+
+      // 부모 요소의 ID에 따라 함수 실행
+      if (parentId === 'to-do-list') {
+        this.delTodo();
+      } else if (parentId === 'complete-list') {
+        this.delComplete();
+      }
     });
     this.comBtnNode.addEventListener('click',()=>{
       this.doneTodo();
     });
   }
+  
   addTodo(){
     const todoList = document.getElementById("to-do-list");
     const input = document.querySelector('input');
@@ -30,6 +40,10 @@ class TodoController {
 
     const completeList = document.getElementById("complete-list");
     completeList.appendChild(this.newTodo.getRow()); //todo-list의 완료된 task complete-list로 붙여넣기
+  }
+  delComplete(){
+    const completeList = document.getElementById("complete-list");
+    completeList.removeChild(this.newTodo.getRow());
   }
 }
 
