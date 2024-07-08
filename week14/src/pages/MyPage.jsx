e1d228a1ce3d13a3c7fcf2f99fbc08aa7d0b3703
eunkong0-0 <joyjessica@naver.com>
2024-07-08 10:46:07 +0900

    Feat : 로그아웃 버튼 및 기능 구현

--- week14/src/pages/MyPage.jsx
+++ week14/src/pages/MyPage.jsx
@@ -1,10 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { getMyPage } from '../apis/user';
+import { useNavigate } from 'react-router-dom';
 
 const MyPage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
+
+  const router = useNavigate();
+
+  //로그아웃 버튼을 누르면 토큰이 제거되고 로그인 페이지로 이동
+  const handleLogout = ()=>{
+    localStorage.removeItem("access")
+    localStorage.removeItem("refresh")
+    router('/');
+  }
 
   useEffect(()=>{
     getMyPage(localStorage.getItem("access"))
@@ -20,11 +30,14 @@ const MyPage = () => {
   if (loading) return <div>로딩중입니당...⌛</div>;
 
   return (
+    <>
     <Wrapper>
       <Title>회원 정보</Title>
       <div>회원님 성함: {data.name}</div>
       <div>회원님 나이: {data.age}</div>
     </Wrapper>
+    <Button onClick={handleLogout}>로그아웃</Button>
+    </>
   )
 }
 
@@ -56,3 +69,23 @@ const Title = styled.div`
   font-weight: 800;
   line-height: normal;
 `;
+
+const Button = styled.div`
+  padding: 10px 20px;
+  background: #4CAF50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  font-family: "OTWelcomeRA";
+  
+  width: 15%;
+  text-align: center;
+  margin: auto;
+  margin-top: 20px;
+
+  &:hover {
+    background: #45a049;
+  }
+  `;
